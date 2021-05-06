a3b81c484ec04c32ec6300105e2a70aeb6c30ac4
Zhang LingYu <zhangly279@chinaunicom.cn>
2021-05-06 15:25:40 +0800

    add accounts page

---
+++ src/pages/Accounts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <b-table striped hover :items="accounts" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: [
+            'aid', 
+            { key:'uid', sortable: true }, 
+            'username', 
+            'email', 
+            { key: 'activationDate', sortable: true }, 
+            { key: 'expireDate', sortable: true }, 
+            'action'
+        ],
+        accounts: [
+          { aid: 'uuid', uid: 'uid', username: 'username', email: 'test@example.com', activationDate: '20xx-xx-xx', expireDate: '20xx-xx-xx'}
+        ]
+      }
+    }
+  }
+</script>
